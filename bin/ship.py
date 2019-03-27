a2d8dae134af1985185d737c17a6b724d1586cdd
Jay Shah <j24shah@gmail.com>
2019-03-26 22:59:55 -0400

    Adding ability to stop ship at screen's end.

--- bin/ship.py
+++ bin/ship.py
@@ -1,35 +1,54 @@
 import os
 import pygame
-from pygame import Surface
 
 
 class Ship():
-    def __init__(self, screen):
-        """ init the ship and set its starting position."""
+    def __init__(self, setting, screen):
+        """
+        Constructor.
+        Init the ship and set its starting position.
+        :param setting: Setting page.
+        :param screen: Game screen.
+        """
 
         self.screen = screen
+        self.setting = setting
 
         # Load the ship image and get its rectangle.
         self.image = pygame.image.load(
             os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'images', 'ship.png')
         )
-        # self.rect = Surface.get_rect(self.image)
-        self.rect = self.image.get_rect()
-        self.screen_rect = screen.get_rect()
-
+        self.rect = self.image.get_rect()  # gets the rect of an image.
+        self.screen_rect = screen.get_rect()  # screens rect.
 
         # Start each new ship at the bottom center of the Screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        # Movement Flag.
-        self.move_right = False
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
+        self.move_right = False  # Movement Flag.
+        self.move_left = False  # Left movement flag.
 
     def update(self):
-        """ Updates the ship's position based on the movement flag."""
-        if self.move_right:
-            self.rect.centerx += 1
+        """
+        Updates the ship's position.
+        :return: Ship's location.
+        """
+        # Updating the ship's center value, not the rect.
+        if self.move_right and self.rect.right < self.screen_rect.right:  # Stops the ship when hit right wall.
+            self.center += self.setting.ship_speed_factor
+
+        elif self.move_left and self.rect.left > 0:  # stops ship when it hits the left wall.
+            self.center -= self.setting.ship_speed_factor
+
+        # Update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
-        # Draw the ship at its current location.
+        """
+        Draws the ship at its current location.
+        :return: ship's location.
+        """
         self.screen.blit(self.image, self.rect)
